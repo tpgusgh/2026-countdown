f2c456bb2e357c9988f40cec236070b4d5d92b33
hyunho <kimhyunhi1@gmail.com>
2025-01-03 13:44:33 +0900

    version 4

--- src/Countdown.jsx
+++ src/Countdown.jsx
@@ -26,51 +26,57 @@ const Countdown = () => {
     return { days, hours, minutes, seconds, milliseconds };
   };
 
+
+
   const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
   const [currentDate, setCurrentDate] = useState(new Date().toLocaleDateString());
+
 
   useEffect(() => {
     const timer = setInterval(() => {
       setTimeLeft(calculateTimeLeft());
-      setCurrentDate(new Date().toLocaleDateString()); // Update date every day
+      setCurrentDate(new Date().toLocaleDateString());
     }, 100);
 
     return () => clearInterval(timer);
   }, []);
 
   return (
-    <div className="countdown">
-      <h1 className="title">Countdown to 2026</h1>
+    <div className={`countdown`}>
+      <h1 className="title">2026 카운트 다운</h1>
       <div className="time-container">
         <div className="time-box">
           <span className="time">{timeLeft.days}</span>
-          <span className="label">Days</span>
+          <span className="label">날</span>
         </div>
         <div className="time-box">
           <span className="time">{timeLeft.hours}</span>
-          <span className="label">Hours</span>
+          <span className="label">시간</span>
         </div>
         <div className="time-box">
           <span className="time">{timeLeft.minutes}</span>
-          <span className="label">Minutes</span>
+          <span className="label">분</span>
         </div>
         <div className="time-box">
           <span className="time">{timeLeft.seconds}</span>
-          <span className="label">Seconds</span>
+          <span className="label">초</span>
         </div>
         <div className="time-box">
           <span className="time">{timeLeft.milliseconds}</span>
-          <span className="label">Milliseconds</span>
+          <span className="label">밀리초</span>
         </div>
       </div>
 
       <div className="current-date">
-        <p>Today's Date: {currentDate}</p>
+        <p>오늘: {currentDate}</p>
       </div>
 
       <div className="github-link">
         <a href="https://github.com/tpgusgh" target="_blank" rel="noopener noreferrer">
-          <button className="github-button"><img className="imgsize" src="/github-mark.svg" /> Go to My GitHub</button>
+          <button className="github-button">
+            <img className="imgsize" src="/github-mark.svg" alt="GitHub" />
+            Go to My GitHub
+          </button>
         </a>
       </div>
     </div>
