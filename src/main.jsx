5c9466dc16c405eba48d068a029c908e8a0d88a8
hyunho <kimhyunhi1@gmail.com>
2025-01-03 20:34:16 +0900

    version 7

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
+import { Analytics } from "@vercel/analytics/react"
 import Countdown from './Countdown.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Countdown />
+    <Analytics />
   </StrictMode>,
 )
