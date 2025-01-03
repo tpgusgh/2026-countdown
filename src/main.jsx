a0fad3c8964333e6a4b69ecc8c109aadf1d469b3
hyunho <kimhyunhi1@gmail.com>
2025-01-03 13:28:19 +0900

    version 3

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
 import Countdown from './Countdown.jsx'
 
 createRoot(document.getElementById('root')).render(
